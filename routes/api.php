9459f13857c4ba27b8b6ca05b12e500d05ee4e7c
azigani <azigani8@gmail.com>
2024-12-09 16:47:52 +0000

    ajout template

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\CategorieController;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\FournisseurController;
+use App\Http\Controllers\Api\CommandeController;
+use App\Http\Controllers\Api\LigneCommandeController;
+use App\Http\Controllers\Api\VenteController;
+use App\Http\Controllers\Api\LigneVenteController;
+use App\Http\Controllers\Api\RoleUserController;
+use App\Http\Controllers\Api\EmplacementController;
+use App\Http\Controllers\Api\FactureController;
+use App\Http\Controllers\Api\InventaireController;
+use App\Http\Controllers\Api\PaiementController;
+use App\Http\Controllers\Api\TransfertController;
+use App\Http\Controllers\Api\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +30,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('categories', CategorieController::class);
+Route::apiResource('articles', ArticleController::class);
+Route::apiResource('fournisseurs', FournisseurController::class);
+Route::apiResource('commandes', CommandeController::class);
+Route::apiResource('ligne-commandes', LigneCommandeController::class);
+Route::apiResource('ventes', VenteController::class);
+Route::apiResource('ligne-ventes', LigneVenteController::class);
+Route::apiResource('roles-users', RoleUserController::class);
+Route::apiResource('emplacements', EmplacementController::class);
+Route::apiResource('factures', FactureController::class);
+Route::apiResource('inventaires', InventaireController::class);
+Route::apiResource('paiements', PaiementController::class);
+Route::apiResource('transferts', TransfertController::class);
+Route::apiResource('users', UserController::class);
